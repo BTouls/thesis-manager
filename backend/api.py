26171eafb581095aec81772bc4a690ee6e946a7c
btouls <billtoulias@gmail.com>
2020-11-07 13:35:06 +0200

    Connected projects to database. Implemented Create, Read, Update, Delete on projects

--- backend/api.py
+++ backend/api.py
@@ -7,11 +7,11 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-
+# Here we initialize database stuff 
 db_connection = MongoClient()
 db = db_connection.get_database("final-project")
 db_users = db.get_collection("users")
-
+db_projects = db.get_collection("projects")
 
 # # The following variable store users
 # users = [
@@ -42,69 +42,118 @@ db_users = db.get_collection("users")
 # ]
 
 # The following variable stores projects
-projects = [
-    {
-        "id": 1,
-        "title": "Web application for managing academic projects",
-        "native_title": "Ylopoihsh efarmoghs gia akadimaikes ergasies",
-        "abstract": "A summary description of project",
-        "abstract_native": "Syntomh perilipsi thematos",
-        "professor": "Iatrelis",
-        "university": "University of Thessaly",
-        "school": "Engineering",
-        "department": "Computer Science",
-        "tags": ["Computers", "Web", "Applications"],
-        "number_of_students": 1,
-        "assigned_on": ["Bill Toulias"],
-        "status": "preparation",
-        "required_courses" : ["CS 101", "CS 102", "Web applications"],
-        "required_knowledge_topics": ["programming", "python", "http", "databases"],
-        "date": "20/10/2020"
-    },
-    {
-        "id": 2,
-        "title": "Agro Sensor project with arduino",
-        "native_title": "Ylopoihsh efarmoghs arduino",
-        "abstract": "A summary description of project",
-        "abstract_native": "Syntomh perilipsi thematos",
-        "professor": "Ventsas",
-        "university": "University of Thessaly",
-        "school": "Engineering",
-        "department": "Electronics",
-        "tags": ["Programming", "Electronics", "Sensors"],
-        "number_of_students": 2,
-        "assigned_on": [],
-        "status": "unassigned",
-        "required_courses" : ["Electronics 101", "C", "Math 101"],
-        "required_knowledge_topics": ["programming", "c", "arduino", "circuits"],
-        "date": "15/10/2020"
-    }
-]
+# projects = [
+#     {
+#         "id": 1,
+#         "title": "Web application for managing academic projects",
+#         "native_title": "Ylopoihsh efarmoghs gia akadimaikes ergasies",
+#         "abstract": "A summary description of project",
+#         "abstract_native": "Syntomh perilipsi thematos",
+#         "professor": "Iatrelis",
+#         "university": "University of Thessaly",
+#         "school": "Engineering",
+#         "department": "Computer Science",
+#         "tags": ["Computers", "Web", "Applications"],
+#         "number_of_students": 1,
+#         "assigned_on": ["Bill Toulias"],
+#         "status": "preparation",
+#         "required_courses" : ["CS 101", "CS 102", "Web applications"],
+#         "required_knowledge_topics": ["programming", "python", "http", "databases"],
+#         "date": "20/10/2020"
+#     },
+#     {
+#         "id": 2,
+#         "title": "Agro Sensor project with arduino",
+#         "native_title": "Ylopoihsh efarmoghs arduino",
+#         "abstract": "A summary description of project",
+#         "abstract_native": "Syntomh perilipsi thematos",
+#         "professor": "Ventsas",
+#         "university": "University of Thessaly",
+#         "school": "Engineering",
+#         "department": "Electronics",
+#         "tags": ["Programming", "Electronics", "Sensors"],
+#         "number_of_students": 2,
+#         "assigned_on": [],
+#         "status": "unassigned",
+#         "required_courses" : ["Electronics 101", "C", "Math 101"],
+#         "required_knowledge_topics": ["programming", "c", "arduino", "circuits"],
+#         "date": "15/10/2020"
+#     }
+# ]
 
+# API CALLS to handle projects
+# ----------------------------
+
+@app.route("/api/projects", methods=["POST"])
+def create_project():
+    # we use a special request object given by flask library which 
+    # contains data that come along with the post request
+
+    project_new = request.json
+    # user_new has the information given for the new user to be created
+    # however we will override the "id" field and assign automatically
+    # a uuid generate by python's uuid system library
+    project_new["id"] = str(uuid.uuid1())
+
+    db_projects.insert_one(project_new)
+    return '{"message": "project with id: ' + project_new["id"] + ' created succesfully"}'
+    
 
 
 @app.route("/api/projects")
 def get_projects():
+    projects = []
+
+    results = db_projects.find({},{"_id":0})
+
+    for result in results:
+        projects.append(result)
+
     return json.dumps(projects, indent=4)
 
-@app.route("/api/projects/by_id/<pid>")
+@app.route("/api/projects/<pid>")
 def get_project_by_id(pid):
-    pid = int(pid)
-    for project in projects:
-        if pid == project["id"]:
-            return json.dumps(project, indent=4)
+    projects = []
     
-    return json.dumps({})
+    results = db_projects.find({"id":pid},{"_id":0})
 
-@app.route("/api/projects/by_professor/<pname>")
-def get_project_by_professor(pname):
+    for result in results:
+        projects.append(result)
 
-    for project in projects:
-        if pname.lower() == project["professor"].lower():
-            return json.dumps(project, indent=4)
+    return json.dumps(projects, indent=4)
+
+@app.route("/api/projects/<pid>", methods=["PUT"])
+def update_project_by_id(pid):
+    project_update = request.json
+    # ensure that the user's uuid remains the same as before
+    project_update["id"] = pid
+    result = db_projects.replace_one({"id":pid},project_update)
+    if result.modified_count > 0: 
+        return '{"message":"Project updated succesfully"}'
+    else:
+        return '{"message":"Nothing to update"}'
+
+@app.route("/api/projects/<pid>", methods=["DELETE"])
+def delete_project_by_id(pid):
+
+    result = db_projects.delete_one({"id":pid})
+    if result.deleted_count > 0: 
+        return '{"message":"Project deleted succesfully"}'
+    else:
+        return '{"message":"Nothing to delete"}'
+
+# @app.route("/api/projects/by_professor/<pname>")
+# def get_project_by_professor(pname):
+
+#     for project in projects:
+#         if pname.lower() == project["professor"].lower():
+#             return json.dumps(project, indent=4)
     
-    return json.dumps({})        
+#     return json.dumps({})        
     
+
+# API CALLS to handle users
+# -------------------------
 
 @app.route("/api/users")
 def get_users():
@@ -165,12 +214,15 @@ def delete_user_by_id(uid):
     else:
         return '{"message":"Nothing to delete"}'
 
-# very dangerous operation delete all users
-# will be removed later
-@app.route("/api/users", methods=["DELETE"])
-def delete_all_users():
-    result = db_users.delete_many({})
-    if result.deleted_count > 0: 
-        return '{"message":"All users deleted!"}'
-    else:
-        return '{"message":"Nothing to delete"}'
+
+
+
+# # very dangerous operation delete all users
+# # will be removed later
+# @app.route("/api/users", methods=["DELETE"])
+# def delete_all_users():
+#     result = db_users.delete_many({})
+#     if result.deleted_count > 0: 
+#         return '{"message":"All users deleted!"}'
+#     else:
+#         return '{"message":"Nothing to delete"}'
